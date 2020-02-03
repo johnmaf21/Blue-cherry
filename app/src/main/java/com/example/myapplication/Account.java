7a9643cb0705ed11a24cbde6e8e26cfad373cf5e
Simon <dalmaso74@gmail.com>
2020-02-03 16:17:31 +0000

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Account.java
@@ -0,0 +1,55 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+        //Initialize the Buttons
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //set home selected
+        bottomNavigationView.setSelectedItemId(R.id.account);
+
+        //set botton selected
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Home.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.help:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Help.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.about_us:
+                        startActivity(new Intent(getApplicationContext()
+                                ,About.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Account.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Help.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        //Initialize the Buttons
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //set help selected
+        bottomNavigationView.setSelectedItemId(R.id.help);
+
+        //set botton selected
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Home.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.help:
+                        return true;
+                    case R.id.about_us:
+                        startActivity(new Intent(getApplicationContext()
+                                ,About.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Account.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
