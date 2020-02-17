f9588d369d39bf2bb65ba1fea230cb7332a6a763
Simon <dalmaso74@gmail.com>
2020-02-17 22:59:12 +0000

    adding the event with the fix bugs

---
+++ app/src/main/java/com/example/myapplication/Events.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Events extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+
+        //Initialize the Buttons
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //set home selected
+        bottomNavigationView.setSelectedItemId(R.id.events);
+
+        //set button selected
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Home.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.help:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Help.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.about_us:
+                        startActivity(new Intent(getApplicationContext()
+                                ,About.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Account.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.events:
+                        return true;
+
+                }
+                return false;
+            }
+        });
+    }
+
+    }
+
